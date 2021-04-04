c279e911d840969722f6aad692461f43ce9f9bfb
linjingchao <417056127@qq.com>
2021-04-05 01:07:27 +0800

    feat: fix 修改jenkins脚本

--- app-deploy/jenkins/pipelinefile.groovy
+++ app-deploy/jenkins/pipelinefile.groovy
@@ -107,10 +107,9 @@ pipeline {
                   tar -xvf deploy.tar.gz
                   rm -f deploy.tar.gz
                   docker rm -f miniso_audio
-                  docker rmi miniso_audio
-                  docker build ./ -t miniso_audio
+                  # docker rmi miniso_audio
+                  # docker build ./ -t miniso_audio
                   docker run -itd --restart=always --name miniso_audio -p 8002:80 miniso_audio
-
                   rm -rf /home/docker/miniso_audio/*
                 ''', execTimeout: 12000000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]
           }
